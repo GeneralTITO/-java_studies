d22910db20cc46d6b1ef7582a06e573129b2c9ae
tito <twiceqbit@gmail.com>
2024-03-04 18:07:47 -0300

    feat: init

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 9;
+
+        if (numero > 10) {
+            System.out.println("numero maior que 10");
+        } else if (numero < 10) {
+            System.out.println("numero menor que 10");
+        } else {
+            System.out.println("numero igual 10");
+        }
+    }
+}
