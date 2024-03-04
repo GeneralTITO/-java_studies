5a2ee5b8e932a9458c664ad327b5453bdc090616
tito <twiceqbit@gmail.com>
2024-03-04 18:15:50 -0300

    feat: estruturas

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,39 @@
 
 public class Main {
     public static void main(String[] args) {
-        int numero = 9;
+        // int numero = 9;
 
-        if (numero > 10) {
-            System.out.println("numero maior que 10");
-        } else if (numero < 10) {
-            System.out.println("numero menor que 10");
-        } else {
-            System.out.println("numero igual 10");
-        }
+        // if (numero > 10) {
+        // System.out.println("numero maior que 10");
+        // } else if (numero < 10) {
+        // System.out.println("numero menor que 10");
+        // } else {
+        // System.out.println("numero igual 10");
+        // }
+
+        // char grade = 'F';
+
+        // switch (grade) {
+        // case 'A':
+        // System.out.println("Excelente!");
+        // break;
+        // case 'B':
+        // System.out.println("media!");
+        // break;
+        // default:
+        // System.out.println("marrapaiz q nota é essa!");
+        // break;
+        // }
+
+        // for (int i = 0; i < 5; i++) {
+        // System.out.println("contador: " + i);
+        // }
+
+        // int contador = 0;
+
+        // while (contador < 5) {
+        //     System.out.println("Contador: " + contador);
+        //     contador++;
+        // }
     }
 }
