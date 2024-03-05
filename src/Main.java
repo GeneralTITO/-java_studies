cfdfa3610c83ab46f13c97b9bd914cdd53b1260a
tito <twiceqbit@gmail.com>
2024-03-05 16:05:08 -0300

    feaT: exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,17 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        var frutas = Arrays.asList("maça", "banana", "cereja").stream();
-        // var primeiraFruta = frutas.filter(f -> f.startsWith("m")).findFirst();
-        // System.out.println(primeiraFruta.get());
 
-        // var frutasUpercase = frutas.map(f -> f.toUpperCase()).toList();
-        // System.out.println(frutasUpercase.get(2));
+        var exemplo = new Exemplo();
+        try {
+            exemplo.verificarSaldo(-1);
+            // exemplo.verificarIdade(5);
+        } catch (saldoInsuficienteException e) {
+            System.out.println("eita, só pode mais 18");
 
-        // var frutasUpercase = frutas.map(String::toUpperCase).toList();
-        // System.out.println(frutasUpercase.get(2));
-
-        // var frutasOrdenadas  = frutas.sorted().toList();
-        // System.out.println(frutasOrdenadas.getFirst());
-
-        // frutas.forEach(f -> System.out.println(f));
-        frutas.forEach(System.out::println);
-
-        /*da pra fazer iterações normais com for e while sem o stream, basicamente ele é para usar 
-         metodos e só funciona uma vez */
-
+        } catch (Exception e) {
+            System.out.println("aha");
+        }
 
     }
 
