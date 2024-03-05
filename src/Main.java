3c05b500e069f575e67b6643c1688a1b1988cd57
tito <twiceqbit@gmail.com>
2024-03-04 21:08:35 -0300

    feat: init

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        // Tipos Primitivos
-        byte assd = 1; /* -128 até 127 */
-        short assaaasdd = 1; /* -32,768 a 32,767 */
-        int assasdaad = 1; /* -2^31 a 2^31-1 */
-        long assasdad = 1L; /* -2^63 a 2^63-1 */
+        var meuCarro = new Carro("toyota", 2000, "laranja");
+        meuCarro.acelerar();
+        meuCarro.freiar();
 
-        float gtbrg = 1.0F;
-        double afth = 1.0; /* por padrão quando é flutuante vira doble, por isso n precisa de sufixo */
-
-        char a = 'A';
-        boolean x = true;
-        var y = false;
-
-        // tipos de referencia
-        // classes
-
-        // interfaces
-        List<String> list = new ArrayList<>();
-
-        int[] intArray = new int[10];
-
-        String[] srtArray = new String[5];
-        System.out.println(intArray.length);
-
+        System.out.println(meuCarro.cor+meuCarro.ano);
     }
 }
