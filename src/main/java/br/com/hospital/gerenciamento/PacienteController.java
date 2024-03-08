3e6f0b6d9656f29e0dd39eebe460126c06a18fc2
tito <twiceqbit@gmail.com>
2024-03-08 10:54:31 -0300

    feat: api

--- src/main/java/br/com/hospital/gerenciamento/PacienteController.java
+++ src/main/java/br/com/hospital/gerenciamento/PacienteController.java
@@ -6,28 +6,26 @@ import java.util.UUID;
 
 import org.springframework.web.bind.annotation.*;
 
-
-
 @RestController
-@RequestMapping("/pacientes")
+@RequestMapping("/api/pacientes")
 public class PacienteController {
     private List<PacienteModel> pacientes = new ArrayList<>();
 
     @PostMapping
-    public PacienteModel adcionarPacienteModel(@RequestBody PacienteModel paciente){
+    public PacienteModel adcionarPacienteModel(@RequestBody PacienteModel paciente) {
         pacientes.add(paciente);
         return paciente;
     }
 
     @GetMapping
-    public List<PacienteModel> ListarPacientes(){
+    public List<PacienteModel> ListarPacientes() {
         return pacientes;
     }
 
     @PutMapping("/{id}")
-    public PacienteModel atualizarPaciente(@PathVariable UUID id, @RequestBody PacienteModel pacienteAtualizado){
-        for (PacienteModel paciente: pacientes){
-            if (paciente.getId().equals(id)){
+    public PacienteModel atualizarPaciente(@PathVariable UUID id, @RequestBody PacienteModel pacienteAtualizado) {
+        for (PacienteModel paciente : pacientes) {
+            if (paciente.getId().equals(id)) {
                 paciente.setNome(pacienteAtualizado.getNome());
                 paciente.setIdade(pacienteAtualizado.getIdade());
                 paciente.setEndereco(pacienteAtualizado.getEndereco());
@@ -39,7 +37,7 @@ public class PacienteController {
     }
 
     @DeleteMapping("/{id}")
-    public void deletarPaciente(@PathVariable UUID id){
+    public void deletarPaciente(@PathVariable UUID id) {
         pacientes.removeIf(paciente -> paciente.getId().equals(id));
     }
 }
